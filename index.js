3df302149c1a112daa4ae66bb7ff22ba8ff9877f
Martin <anastaciocruzmartin@gmail.com>
2020-11-02 12:38:54 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+import dotenv from 'dotenv';
+
+dotenv.config({ path: 'variables.env' });
+
+const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+   .then( () => console.log('Base de datos conectada') )
+   .catch( error => console.log(error) )
+
+/** Puerto y host para la app */
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 4000;
+
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Obtener el año actual
+app.use( (req, res, next) => {
+   const year = new Date();
+   res.locals.actualYear = year.getFullYear();
+   res.locals.nombresitio = "Agencia de Viajes";
+   return next();
+});
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar Router
+app.use('/', router);
+
+app.listen(port, host, () => {
+   console.log('El servidor esta funcionando');
+});
